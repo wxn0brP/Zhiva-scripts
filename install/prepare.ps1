1dbb10591b1e608e378821c678cd0301c1ea53f4
wxn0brP <toja.filiper@gmail.com>
2025-10-25 14:04:59 +0200

    fix: install bun

--- install/prepare.ps1
+++ install/prepare.ps1
@@ -1,25 +1,18 @@
 if (-not (Get-Command bun -ErrorAction SilentlyContinue)) {
     Write-Host "[Z-SCR-6-01] bun is not installed."
-    $choice = Read-Host "Do you want to install bun? (y/n)"
-    if ($choice -match "^[Yy]$") {
-        Write-Host "[Z-SCR-6-02] Installing bun..."
-        irm https://bun.sh/install.ps1 | iex
-        $env:Path += ";$HOME\.bun\bin"
-        Write-Host "[Z-SCR-6-03] Added ~/.bun/bin to PATH for current session."
-        Write-Host "[Z-SCR-6-04] 💜 Don't forget to add '$HOME\.bun\bin' to your PATH environment variable."
-    } else {
-        Write-Host "[Z-SCR-6-05] Skipped bun installation."
-        exit 1
-    }
+    irm https://bun.sh/install.ps1 | iex
+    $env:Path += ";$HOME\.bun\bin"
+    Write-Host "[Z-SCR-6-02] Added ~/.bun/bin to PATH for current session."
+    Write-Host "[Z-SCR-6-03] 💜 Don't forget to add '$HOME\.bun\bin' to your PATH environment variable."
 } else {
-    Write-Host "[Z-SCR-6-06] 💜 bun is installed."
+    Write-Host "[Z-SCR-6-04] 💜 bun is installed."
 }
 
 if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
-    Write-Host "[Z-SCR-6-07] Error: git is not installed. Please install git manually."
+    Write-Host "[Z-SCR-6-05] Error: git is not installed. Please install git manually."
     exit 1
 } else {
-    Write-Host "[Z-SCR-6-08] 💜 git is installed."
+    Write-Host "[Z-SCR-6-06] 💜 git is installed."
 }
 
 $zhivaPath = Join-Path $HOME ".zhiva"
@@ -35,7 +28,7 @@ if (-not (Test-Path $zhivaScriptsPath)) {
 
 Set-Location (Join-Path $zhivaScriptsPath "zhiva")
 bun install --production --force
-Write-Host "[Z-SCR-6-09] 💜 Zhiva-scripts is installed."
+Write-Host "[Z-SCR-6-07] 💜 Zhiva-scripts is installed."
 
 $cmdContentStartup = @"
 @echo off
@@ -50,4 +43,4 @@ bun run "%USERPROFILE%\.zhiva\scripts\zhiva\install.ts" %*
 $cmdContentStartup | Set-Content -Path (Join-Path $zhivaBinPath "zhiva-startup.cmd") -Force
 $cmdContentInstall | Set-Content -Path (Join-Path $zhivaBinPath "zhiva-install.cmd") -Force
 
-Write-Host "[Z-SCR-6-10] 💜 Zhiva-startup is installed."
+Write-Host "[Z-SCR-6-08] 💜 Zhiva-startup is installed."
