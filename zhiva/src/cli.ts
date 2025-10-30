bd3c875136bfea170712b7dfa1aaa15550f7337b
wxn0brP <toja.filiper@gmail.com>
2025-10-30 21:22:23 +0100

    feat: add aliases

--- zhiva/src/cli.ts
+++ zhiva/src/cli.ts
@@ -1,11 +1,23 @@
 #!/usr/bin/env bun
 
-const name = process.argv[2];
+let name = process.argv[2];
 if (!name) {
     console.error("Please provide an module name");
     process.exit(1);
 }
 process.argv.splice(2, 1);
+
+const aliases = {
+    start: ["run", "startup", "o", "r"],
+    install: ["i", "add"],
+}
+
+for (const key of Object.keys(aliases)) {
+    if (aliases[key].includes(name)) {
+        name = key;
+        break;
+    }
+}
 
 await import("./" + name);
 
