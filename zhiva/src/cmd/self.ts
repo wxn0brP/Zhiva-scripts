7d70c729007c229e284a1e9387c4b57061dce2d9
wxn0brP <toja.filiper@gmail.com>
2025-11-02 14:59:59 +0100

    fix: self update

--- zhiva/src/cmd/self.ts
+++ zhiva/src/cmd/self.ts
@@ -6,7 +6,7 @@ import { join } from "path";
 
 process.chdir(join(homedir(), ".zhiva"));
 for (const name of ["deps", "engine"]) {
-    await import("./utils/" + name);
+    await import("../utils/" + name);
 }
 writeFileSync("latest-check", String(Date.now()));
 console.log("[Z-SCR-7-01] 💜 Update completed");
