8def67b2e3b36d08b7a97261025094801d3d9891
wxn0brP <toja.filiper@gmail.com>
2025-12-28 17:19:43 +0100

    feat: update format

--- src/cmd/search.ts
+++ src/cmd/search.ts
@@ -1,23 +1,23 @@
 import { getFromCache } from "../utils/cache";
 import { _guessApp } from "../utils/guess";
-import { parseToml } from "../utils/toml";
 
 interface Repo {
-    full_name: string;
-    description: string;
-    stargazers_count: number;
+    name: string;
+    desc: string;
+    stars: number;
     verified?: boolean;
 }
 
 export default async (args: string[]) => {
     const name = args.find((arg) => !arg.startsWith("-")) || "";
     const jsonMode = args.includes("--json");
+    const prettyMode = args.includes("--pretty");
 
     let apps = await getFromCache("search", 5 * 60 * 1000, fetchAllRepos);
     const banned = await getFromCache("banned", 5 * 60 * 1000, fetchBanned);
 
-    apps = apps.filter(item => !banned.includes(item.full_name));
-    const results = await _guessApp(name, apps.map(item => item.full_name));
+    apps = apps.filter(item => !banned.includes(item.name));
+    const results = await _guessApp(name, apps.map(item => item.name));
 
     if (!jsonMode) {
         console.log(results.join("\n"));
@@ -29,37 +29,36 @@ export default async (args: string[]) => {
         5 * 60 * 1000,
         fetchVerified,
         [
-            apps.map(item => item.full_name)
+            apps.map(item => item.name)
         ]
     );
 
-    apps = apps.filter(item => results.includes(item.full_name));
-    apps.forEach(item => item.verified = verified.includes(item.full_name));
+    apps = apps.filter(item => results.includes(item.name));
+    apps.forEach(item => item.verified = verified.includes(item.name));
 
-    console.log(JSON.stringify(apps));
+    console.log(JSON.stringify(apps, null, prettyMode ? 2 : undefined));
 }
 
 async function fetchAllRepos(): Promise<Repo[]> {
-    const data = await fetch("https://api.github.com/search/repositories?q=topic:Zhiva-app").then((res) => res.json()) as { items: Repo[] };
-    return data.items.map(item => ({
-        full_name: item.full_name,
-        description: item.description,
-        stargazers_count: item.stargazers_count
+    const data = await fetch("https://api.github.com/search/repositories?q=topic:Zhiva-app").then((res) => res.json());
+    return data.items.map((item: any) => ({
+        name: item.full_name,
+        desc: item.description,
+        stars: item.stargazers_count
     }));
 }
 
-async function fetchTOML(file: string) {
-    const data = await fetch(`https://raw.githubusercontent.com/wxn0brP/Zhiva-registry/master/${file}.toml`).then((res) => res.text());
-    return parseToml(data);
+async function fetchData(file: string) {
+    const data = await fetch(`https://raw.githubusercontent.com/wxn0brP/Zhiva-registry/HEAD/${file}.txt`).then((res) => res.text());
+    return data.split("\n").map(line => line.split("#")[0].trim()).filter(Boolean);
 }
 
 async function fetchBanned() {
-    return Object.keys(await fetchTOML("banned"));
+    return await fetchData("banned");
 }
 
 async function fetchVerified(installedApps: string[]) {
-    const data = await fetchTOML("verified");
-    const apps = Object.keys(data);
+    const apps = await fetchData("verified");
     apps.push(...installedApps.filter(app => app.startsWith("wxn0brP/")));
     return apps;
 }
